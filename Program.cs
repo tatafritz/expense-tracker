4d2b34c9f465974414a8ea082dbcb6a05db84596
Tarsila Barros Fritz <tarsilabarrosfritz03@gmail.com>
2026-09-20 12:29:56 -0300

    feat: add initial expense creation and listing
    start of the project and my first steps in learning how to work with classes, objects, and lists in C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Expense> expenses = new List<Expense>();
+        Expense expense = new Expense();
+
+        expense.Id = expenses.Count + 1;
+
+        Console.Write("Amount: ");
+        expense.Amount = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Description: ");
+        expense.Description = Console.ReadLine();
+
+        Console.Write("Category: ");
+        expense.Category = Console.ReadLine();
+
+        expense.Date = DateTime.Now;
+
+        expenses.Add(expense);
+
+        foreach (Expense currentExpense in expenses)
+        {
+            Console.WriteLine($"Id: {currentExpense.Id}");
+            Console.WriteLine($"Amount: {currentExpense.Amount}");
+            Console.WriteLine($"Description: {currentExpense.Description}");
+            Console.WriteLine($"Category: {currentExpense.Category}");
+            Console.WriteLine($"Date: {currentExpense.Date}");
+        }
+    }
+}
